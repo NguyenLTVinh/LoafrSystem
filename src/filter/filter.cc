629c27314a455953799452a20073016c8f443177
joey-langevin <joey.langevin44@gmail.com>
2023-11-14 20:21:39 -0600

    filter changes

--- src/filter/filter.cc
+++ src/filter/filter.cc
@@ -1,10 +1,12 @@
 #include "logentry.h"
 
 // Implementation of the LogEntry constructor
-LogEntry::LogEntry(const std::string& time, const std::string& comp, const std::string& event,
+Filter::Filter(const std::string& time, const std::string& comp, const std::string& event,
                    const std::string& field, int value)
     : timeStamp(time),
       componentName(comp),
       eventType(event),
       dataFieldName(field),
       dataValue(value) {}
+
+Filter:: Filter(const vector<std::string>& logEntries, const std::string&, const std::string& operator, const int val);
--- src/filter/filter.h
+++ src/filter/filter.h
@@ -3,9 +3,10 @@
 
 #include <string>
 #include <json/json.h> 
+#include <vector>
 
 
-class LogEntry {
+class Filter {
     public: 
     std::string timeStamp;
     std::string componentName;
@@ -15,8 +16,9 @@ class LogEntry {
 
 
     // Constructor for convenience
-    LogEntry(const std::string& time, const std::string& comp, const std::string& event,
+    Filter(const std::string& time, const std::string& comp, const std::string& event,
              const std::string& field, int value);
 };
+    Filter(const vector<std::string>& logEntries, const std::string&, const std::string& operator, const int val);
 
 #endif
