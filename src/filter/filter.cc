68afd486c9e8dd96484ad4cbd471ba2eca03e6e4
Vinh Nguyen <nguy4766@umn.edu>
2023-11-20 21:01:52 -0600

    bug fixes for filter

--- src/filter/filter.cc
+++ src/filter/filter.cc
@@ -7,39 +7,39 @@
 
 Filter::Filter() {}
 
-Filter::Filter(const std::string& time, const std::string& comp,
-               const std::string& event, const std::string& field, int value)
-    : timeStamp(time),
-      componentName(comp),
-      eventType(event),
-      dataFieldName(field),
-      dataValue(value) {}
-
 std::vector<std::string> Filter::FilterLog(
     const std::vector<std::string>& logEntries, const std::string& logItem,
     const std::string& operation, const int val) {
-  std::vector<std::string> matchedEntries;
-  for (const auto& entry : logEntries) {
-    std::istringstream iss(entry);
-    std::vector<std::string> entryItems;
-    std::string item;
+    std::vector<std::string> matchedEntries;
 
-    while (std::getline(iss, item, ',')) {
-      entryItems.push_back(item);
-    }
+    for (const auto& entry : logEntries) {
+        if (entry.find(logItem) != std::string::npos) {
+            std::istringstream iss(entry);
+            std::vector<std::string> entryItems;
+            std::string item;
 
-    if (!entryItems.empty()) {
-      try {
-        int logItemInteger = std::stoi(entryItems.back());
-        if ((operation == "<" && val > logItemInteger) ||
-            (operation == "=" && val == logItemInteger) ||
-            (operation == ">" && val < logItemInteger)) {
-          matchedEntries.push_back(entry);
+            while (std::getline(iss, item, ',')) {
+                entryItems.push_back(item);
+            }
+
+            if (!entryItems.empty()) {
+                try {
+                    int logItemValue = std::stoi(entryItems.back());
+                    if ((operation == "<" && logItemValue < val) ||
+                        (operation == "=" && logItemValue == val) ||
+                        (operation == ">" && logItemValue > val)) {
+                        matchedEntries.push_back(entry);
+                    }
+                } catch (const std::invalid_argument&) {
+                    std::cerr << "Invalid value for comparison in the last column of the entry.\n";
+                }
+            }
         }
-      } catch (std::invalid_argument& e) {
-        std::cerr << "Non-integer value in Data Value field.\n";
-      }
     }
-  }
-  return matchedEntries;
+
+    if (matchedEntries.empty()) {
+        std::cerr << "No entries matched the filter criteria.\n";
+    }
+
+    return matchedEntries;
 }
