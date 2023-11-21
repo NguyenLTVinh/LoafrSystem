024eff5c99c0d558cd639f95d3eaa4a93538d83a
joey-langevin <joey.langevin44@gmail.com>
2023-11-20 21:28:48 -0600

    setup doxygen

---
+++ docs/html/search/classes_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['negation_0',['negation',['../structdetail_1_1negation.html',1,'detail']]],
+  ['newdataentry_1',['NewDataEntry',['../class_new_data_entry.html',1,'']]],
+  ['nonesuch_2',['nonesuch',['../structdetail_1_1nonesuch.html',1,'detail']]]
+];
---
+++ docs/html/search/classes_f.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['search_0',['Search',['../class_search.html',1,'']]],
+  ['serializer_1',['serializer',['../classdetail_1_1serializer.html',1,'detail']]],
+  ['sort_2',['Sort',['../class_sort.html',1,'']]],
+  ['span_5finput_5fadapter_3',['span_input_adapter',['../classdetail_1_1span__input__adapter.html',1,'detail']]],
+  ['static_5fconst_4',['static_const',['../structdetail_1_1static__const.html',1,'detail']]]
+];
