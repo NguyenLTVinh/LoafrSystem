8ad57173e84eab75b4209eb0f2a82647a8e73daa
Vinh Nguyen <nguy4766@umn.edu>
2023-11-20 21:40:39 -0600

    code styles fix

--- src/model/LoafrModel.h
+++ src/model/LoafrModel.h
@@ -7,23 +7,25 @@
 #define LOAFRMODEL_H
 
 #include <memory>
-#include "filter/filter.h"
+
 #include "NewDataEntry/NewDataEntry.h"
+#include "filter/filter.h"
 #include "search/Search.h"
 #include "sort/Sort.h"
 
 /**
  * @class LoafrModel
- * @brief A model class that encapsulates filtering, searching, and sorting functionalities for log entries.
+ * @brief A model class that encapsulates filtering, searching, and sorting
+ * functionalities for log entries.
  *
  * This class uses unique pointers to manage Filter, Search, and Sort objects,
  * providing a unified interface for processing log file entries.
  */
 class LoafrModel {
  private:
-  std::unique_ptr<Filter> filter; ///< Unique pointer to a Filter object.
-  std::unique_ptr<Search> search; ///< Unique pointer to a Search object.
-  std::unique_ptr<Sort> sort;     ///< Unique pointer to a Sort object.
+  std::unique_ptr<Filter> filter;  ///< Unique pointer to a Filter object.
+  std::unique_ptr<Search> search;  ///< Unique pointer to a Search object.
+  std::unique_ptr<Sort> sort;      ///< Unique pointer to a Sort object.
 
  public:
   /**
@@ -38,18 +40,20 @@ class LoafrModel {
 
   /**
    * @brief Saves log entries as a JSON file.
-   * 
+   *
    * @param logEntries A vector of string containing log entries to be saved.
    * @param path The directory path where the JSON file will be saved.
-   * @param baseFileName The base name of the file used to create the output file name.
+   * @param baseFileName The base name of the file used to create the output
+   * file name.
    */
   void saveLogEntriesAsJson(const std::vector<std::string>& logEntries,
                             const std::string& path,
                             const std::string& baseFileName);
 
   /**
-   * @brief Filters log entries based on specified criteria and saves the results as JSON.
-   * 
+   * @brief Filters log entries based on specified criteria and saves the
+   * results as JSON.
+   *
    * @param newDataEntry The NewDataEntry object containing log entries.
    * @param logItem The field name to filter on.
    * @param operation The comparison operation ("<", "=", ">").
@@ -62,10 +66,11 @@ class LoafrModel {
 
   /**
    * @brief Searches for a keyword in log entries and saves the results as JSON.
-   * 
+   *
    * @param keyword The keyword to search for.
    * @param newDataEntry The NewDataEntry object containing log entries.
-   * @param path The directory path where the search results JSON file will be saved.
+   * @param path The directory path where the search results JSON file will be
+   * saved.
    */
   void SaveSearchKeyword(const std::string& keyword,
                          const NewDataEntry& newDataEntry,
@@ -73,7 +78,7 @@ class LoafrModel {
 
   /**
    * @brief Sorts log entries by timestamp and saves them as JSON.
-   * 
+   *
    * @param sortType The type of sorting ("ascending" or "descending").
    * @param newDataEntry The NewDataEntry object containing log entries.
    * @param path The directory path where the sorted JSON file will be saved.
