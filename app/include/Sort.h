319f74ae8f1fef9bae74a21c707d529d0b24f56c
Zakariya <abdi0356@umn.edu>
2023-11-22 17:21:40 -0600

    sort test and docs

--- app/include/Sort.h
+++ app/include/Sort.h
@@ -4,10 +4,25 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Sort class provides functionalities to sort the log files.
+ * This class sorts log entries based on ascending or descending order of
+ * timestamps.
+ */
 class Sort {
  private:
  public:
+  /**
+   * @brief Constructor for the sort class
+   */
   Sort();
+
+  /**
+   * @brief Sorts log entries based on the specified sorting type.
+   * @param sortType The type of sorting (ascending or descending).
+   * @param fileContents The vector containing the data being sorted.
+   * @return std::vector<std::string> The sorted log entries.
+   */
   std::vector<std::string> sortFile(const std::string& sortType,
                                     std::vector<std::string>& fileContents);
 };
