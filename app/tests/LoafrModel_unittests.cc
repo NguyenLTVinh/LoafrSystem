792038aeae3d5e30ef2a80b9a737beb5304254de
Vinh Nguyen <nguy4766@umn.edu>
2023-11-22 11:17:17 -0600

    tests for model updated

--- app/tests/LoafrModel_unittests.cc
+++ app/tests/LoafrModel_unittests.cc
@@ -2,7 +2,6 @@
 #include "LoafrModel.h"
 #include <filesystem>
 #include <fstream>
-#include <nlohmann/json.hpp>
 
 class LoafrModelTest : public ::testing::Test {
 protected:
@@ -16,7 +15,7 @@ protected:
 TEST_F(LoafrModelTest, SaveEmptyLogEntries) {
     std::vector<std::string> logEntries;
     std::string path = "/tmp";
-    std::string baseFileName = "testLog.json";
+    std::string baseFileName = "testLog.txt";
     model.saveLogEntriesAsJson(logEntries, path, baseFileName);
     std::string expectedFileName = path + "/testLog-out.json";
     ASSERT_TRUE(std::filesystem::exists(expectedFileName));
@@ -28,7 +27,7 @@ TEST_F(LoafrModelTest, SaveNonEmptyLogEntries) {
         "2023-01-02, Component2, Event2, Field2, Value2"
     };
     std::string path = "/tmp";
-    std::string baseFileName = "testLog.json";
+    std::string baseFileName = "testLog.txt";
     model.saveLogEntriesAsJson(logEntries, path, baseFileName);
     std::string expectedFileName = path + "/testLog-out.json";
     ASSERT_TRUE(std::filesystem::exists(expectedFileName));
@@ -36,34 +35,25 @@ TEST_F(LoafrModelTest, SaveNonEmptyLogEntries) {
 
 TEST_F(LoafrModelTest, VerifyOutputFileContent) {
     std::vector<std::string> logEntries = {
-        "2023-01-01,Component1,Event1,Field1,Value1",
-        "2023-01-02,Component2,Event2,Field2,Value2"
+        "2023-01-01, Component1, Event1, Field1, Value1",
+        "2023-01-02, Component2, Event2, Field2, Value2"
     };
     std::string path = "/tmp";
-    std::string baseFileName = "testLog.json";
+    std::string baseFileName = "testLogVerify.txt";
     model.saveLogEntriesAsJson(logEntries, path, baseFileName);
-    std::string expectedFileName = path + "/testLog-out.json";
-    std::ifstream inFile(expectedFileName);
-    ASSERT_TRUE(inFile.is_open());
-    std::stringstream buffer;
-    buffer << inFile.rdbuf();
-    std::string fileContent = buffer.str();
-    nlohmann::json parsedJson = nlohmann::json::parse(fileContent);
-    nlohmann::json expectedJson = {
-        {
-            {"Timestamp", "2023-01-01"},
-            {"Component Name", "Component1"},
-            {"Event Type", "Event1"},
-            {"Data Field Name", "Field1"},
-            {"Data Value", "Value1"}
-        },
-        {
-            {"Timestamp", "2023-01-02"},
-            {"Component Name", "Component2"},
-            {"Event Type", "Event2"},
-            {"Data Field Name", "Field2"},
-            {"Data Value", "Value2"}
-        }
+    std::string expectedFileName = path + "/testLogVerify-out.json";
+    std::ifstream outFile(expectedFileName);
+    ASSERT_TRUE(outFile.is_open());
+    std::string fileContentStr((std::istreambuf_iterator<char>(outFile)), std::istreambuf_iterator<char>());
+
+    // Function to replace newlines and tabs with spaces and remove extra spaces
+    auto formatJsonString = [](std::string& str) {
+        std::replace(str.begin(), str.end(), '\n', ' ');
+        std::replace(str.begin(), str.end(), '\t', ' ');
+        str.erase(std::unique(str.begin(), str.end(), [](char a, char b) { return a == ' ' && b == ' '; }), str.end());
     };
-    ASSERT_EQ(parsedJson, expectedJson);
+    formatJsonString(fileContentStr);
+    std::string jsonStr = R"([ { "Component Name": "Component1", "Data Field Name": "Field1", "Data Value": "Value1", "Event Type": "Event1", "Timestamp": "2023-01-01" }, { "Component Name": "Component2", "Data Field Name": "Field2", "Data Value": "Value2", "Event Type": "Event2", "Timestamp": "2023-01-02" } ])";
+    ASSERT_EQ(jsonStr, fileContentStr);
+
 }
