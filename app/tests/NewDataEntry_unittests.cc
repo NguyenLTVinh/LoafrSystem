d5d7a0236191ae3a29600d0538b8da2a642bce47
Amaar Mohamed <codebyamaar@gmail.com>
2023-11-21 22:02:46 -0600

    inital tests for NewDataEntry class

--- app/tests/NewDataEntry_unittests.cc
+++ app/tests/NewDataEntry_unittests.cc
@@ -1,20 +1,26 @@
-#include <gtest/gtest.h>
 #include "NewDataEntry.h"
+#include <gtest/gtest.h>
 
 class NewDataEntryTest : public ::testing::Test {
 public:
   void SetUp() {
     // code here will execute just before the test ensues
+    test1 = new NewDataEntry("temp1.txt", "../tests");
+    test2 = new NewDataEntry("example1.txt", "");
   }
   void TearDown() {}
 
 protected:
+  NewDataEntry *test1;
+  NewDataEntry *test2;
 };
 
-// Demonstrate some basic assertions.
-TEST(NewDataEntryTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST_F(NewDataEntryTest, GetFileNameTest) {
+  EXPECT_EQ(test1->getFileName(), "temp1.txt");
+  EXPECT_EQ(test2->getFileName(), "example1.txt");
+}
+
+TEST_F(NewDataEntryTest, GetPathTest) {
+  EXPECT_EQ(test1->getPath(), "../tests");
+  EXPECT_EQ(test2->getPath(), "");
 }
