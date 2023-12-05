e5034a2a87cd223ec6c1f43e05620254d2eb5379
Vinh Nguyen <nguy4766@umn.edu>
2023-12-05 15:21:30 -0600

    added filter functions to filter between start and end events #14

--- app/include/filter.h
+++ app/include/filter.h
@@ -11,7 +11,8 @@
  * well as an integer to compare against the integers in the inputted logfiles.
  * Only the logEntries that contain both the keyword and satisfy the operation
  * will be returned.
- * Traceability: This class is traced to Section 3.3.10: Filter Class on the Design Document
+ * Traceability: This class is traced to Section 3.3.10: Filter Class on the
+ * Design Document
  */
 class Filter {
  public:
@@ -32,6 +33,31 @@ class Filter {
                                      const std::string& logItem,
                                      const std::string& operation,
                                      const int val);
+  /**
+   * @brief Filters log entries into groups based on start and end event names.
+   *
+   * This function takes a vector of log entries and two strings representing
+   * the names of start and end events. It groups the log entries from each
+   * start event to the subsequent end event. If a start event does not have a
+   * corresponding end event, it includes all subsequent log entries until the
+   * end of the logEntries vector. The function ensures that the values
+   * associated with start and end events are -1 and prints an error to
+   * std::cerr if they are not.
+   *
+   * @param logEntries A vector of strings, each representing a log entry.
+   * @param StartEventName The name of the start event to look for in the log
+   * entries.
+   * @param EndEventName The name of the end event to look for in the log
+   * entries.
+   * @return A vector of string vectors, where each inner vector represents a
+   * group of log entries from a start event to an end event. Returns an empty
+   * vector if an error occurs (e.g., if the value associated with a start or
+   * end event is not -1).
+   *
+   */
+  std::vector<std::vector<std::string>> FilterByStartEndEvents(
+      const std::vector<std::string>& logEntries,
+      const std::string& StartEventName, const std::string& EndEventName);
 };
 
 #endif
--- app/src/filter.cc
+++ app/src/filter.cc
@@ -43,3 +43,53 @@ std::vector<std::string> Filter::FilterLog(
 
   return matchedEntries;
 }
+
+std::vector<std::vector<std::string>> Filter::FilterByStartEndEvents(
+    const std::vector<std::string>& logEntries,
+    const std::string& StartEventName, const std::string& EndEventName) {
+  std::vector<std::vector<std::string>> groupedEntries;
+  std::vector<std::string> currentGroup;
+  bool isGrouping = false;
+
+  for (const auto& entry : logEntries) {
+    std::istringstream iss(entry);
+    std::vector<std::string> entryItems;
+    std::string item;
+    while (std::getline(iss, item, ',')) {
+      entryItems.push_back(item);
+    }
+
+    // Check for start event
+    if (entryItems[2] == StartEventName) {
+      if (entryItems[4] != "-1") {
+        std::cerr << "Error: Start event value not -1" << std::endl;
+        return std::vector<std::vector<std::string>>();  // Return empty vector
+      }
+      if (isGrouping) {
+        // Handle previous group if it was not closed
+        groupedEntries.push_back(currentGroup);
+        currentGroup.clear();
+      }
+      isGrouping = true;
+      currentGroup.push_back(entry);
+    } else if (isGrouping && entryItems[2] == EndEventName) {
+      if (entryItems[4] != "-1") {
+        std::cerr << "Error: End event value not -1" << std::endl;
+        return std::vector<std::vector<std::string>>();  // Return empty vector
+      }
+      currentGroup.push_back(entry);
+      groupedEntries.push_back(currentGroup);
+      currentGroup.clear();
+      isGrouping = false;
+    } else if (isGrouping) {
+      currentGroup.push_back(entry);
+    }
+  }
+
+  if (isGrouping) {
+    // Handle last group if it was not closed
+    groupedEntries.push_back(currentGroup);
+  }
+
+  return groupedEntries;
+}
